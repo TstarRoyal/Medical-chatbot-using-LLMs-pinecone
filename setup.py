ee5e400316979337bce5bf31d6982c1d85bf3766
Tejas Khade <160311022+TstarRoyal@users.noreply.github.com>
2026-01-21 10:56:09 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Tejas khade",
+    author_email="tejaskhade711@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
